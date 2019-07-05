bccacc8f55c2061e787043405579b509ad5cc335
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-07-05 09:49:47 -0300

    Repositório de Atendente

---
+++ src/main/java/br/ufjf/dcc193/tbrop/repository/AtendenteRepository.java
@@ -0,0 +1,8 @@
+package br.ufjf.dcc193.tbrop.repository;
+
+import br.ufjf.dcc193.tbrop.model.Atendente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtendenteRepository extends JpaRepository<Atendente, Long> {
+
+}
