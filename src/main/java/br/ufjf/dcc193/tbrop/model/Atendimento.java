a4230723085c8ee0ee55bd21d8ba87eb7714c9b2
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-07-05 01:16:35 -0300

    Modelo de Atendimento

---
+++ src/main/java/br/ufjf/dcc193/tbrop/model/Atendimento.java
@@ -0,0 +1,166 @@
+package br.ufjf.dcc193.tbrop.model;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class Atendimento implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Categoria categoria;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataCriacao;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataFechamento;
+
+    @OneToOne
+    private Atendente atendente;
+
+    @OneToOne
+    private Usuario usuario;
+
+    private int status;
+
+    @OneToMany
+    private List<Evento> eventos;
+    
+    private String descricao;
+
+    public Atendimento() {
+    }
+
+    public Atendimento(Long id, Categoria categoria, Date dataCriacao, Date dataFechamento, Atendente atendente, Usuario usuario, int status, List<Evento> eventos, String descricao) {
+        this.id = id;
+        this.categoria = categoria;
+        this.dataCriacao = dataCriacao;
+        this.dataFechamento = dataFechamento;
+        this.atendente = atendente;
+        this.usuario = usuario;
+        this.status = status;
+        this.eventos = eventos;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getDataCriacaoBR() {
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        return formato.format(dataCriacao);
+    }
+
+    public Date getDataFechamento() {
+        return dataFechamento;
+    }
+
+    public void setDataFechamento(Date dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
+
+    public String getDataFechamentoBR() {
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        return formato.format(dataFechamento);
+    }
+
+    public Atendente getAtendente() {
+        return atendente;
+    }
+
+    public void setAtendente(Atendente atendente) {
+        this.atendente = atendente;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getStatusNome() {
+        switch (status) {
+            case 1:
+                return "Em revisão";
+            case 2:
+                return "Aberto";
+            case 3:
+                return "Bloqueado";
+            case 4:
+                return "Em andamento";
+            case 5:
+                return "Fechado";
+            default:
+                return "";
+        }
+    }
+
+    public List<Evento> getEventos() {
+        return eventos;
+    }
+
+    public void setEventos(List<Evento> eventos) {
+        this.eventos = eventos;
+    }
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Atendimento{" + "id=" + id + ", categoria=" + categoria + ", dataCriacao=" + dataCriacao + ", dataFechamento=" + dataFechamento + ", atendente=" + atendente + ", usuario=" + usuario + ", status=" + status + ", eventos=" + eventos + ", descricao=" + descricao + '}';
+    }
+}
