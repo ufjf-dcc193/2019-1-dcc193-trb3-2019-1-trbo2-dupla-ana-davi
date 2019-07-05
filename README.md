7b016ba2bf932f403568b3fca60b8dcc86904178
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-07-05 00:02:22 -0300

    Configurações iniciais do projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+"# 2019-1-dcc193-trb3-2019-1-trbo2-dupla-ana-davi" 
---
+++ src/main/java/br/ufjf/dcc193/tbrop/TrabalhoOpcionalApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.tbrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoOpcionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoOpcionalApplication.class, args);
+	}
+
+}
