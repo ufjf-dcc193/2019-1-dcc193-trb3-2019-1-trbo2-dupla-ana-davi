9f247d5b50ae2b1be9e62d84da387ca93bfc634b
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-07-09 20:14:17 -0300

    CRUD de categorias

--- src/main/resources/static/js/trabalho.js
+++ src/main/resources/static/js/trabalho.js
@@ -1,7 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$(document).ready(function () {
+    $('#tbCategorias').DataTable({
+        "language": {
+            "search": "Buscar:",
+            "lengthMenu": "Exibir _MENU_ registros por página",
+            "zeroRecords": "Nenhuma registro encontrado",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "Nenhuma registro disponível",
+            "infoFiltered": "(filtrado de _MAX_ registros totais)",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+        },
+        'paging': true,
+        'lengthChange': true,
+        'searching': true,
+        'ordering': true,
+        'info': true,
+        'autoWidth': false,
+        "columns": [
+            null,
+            {"orderable": false, className: "text-center"}
+        ]
+    });
+});
 
