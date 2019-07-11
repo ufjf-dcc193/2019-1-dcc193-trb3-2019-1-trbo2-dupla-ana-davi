e79f26c451516cde8a21143bb52824080e8f928f
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-07-11 05:14:06 -0300

    Página principal do atendente logado

---
+++ src/main/java/br/ufjf/dcc193/tbrop/controller/admin/AdminController.java
@@ -0,0 +1,29 @@
+package br.ufjf.dcc193.tbrop.controller.admin;
+
+import br.ufjf.dcc193.tbrop.model.Atendente;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+
+    @RequestMapping("/admin")
+    public String index(Model model, HttpSession session) {
+        if (isLogado(session)) {
+            Atendente adminUser = (Atendente) session.getAttribute("adminUser");
+            model.addAttribute("adminUser", adminUser);
+            return "admin/index";
+        }else{
+            return "redirect:/logout";
+        }
+
+    }
+
+    private boolean isLogado(HttpSession session) {
+        Atendente adminUser = (Atendente) session.getAttribute("adminUser");
+        return adminUser != null;
+    }
+
+}
